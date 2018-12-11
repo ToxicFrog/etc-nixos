7518ba4dedd9f2bde6500d5c83253d2426ded7f7
B. Kelly <bk@ancilla.ca>
2018-12-10 19:35:48 -0500

    Rewrite borg configuration
    It now backs up thoth, durandal, and the three websites

--- services/borgbackup.nix
+++ services/borgbackup.nix
@@ -4,39 +4,102 @@
 
 let
   secrets = (import ../secrets/default.nix {});
+  borgcfg = pkgs.copyPathToStore ../borg;
   borg = { name, ... } @ opts: ({
     archiveBaseName = name;
-    extraCreateArgs = "--stats --progress --exclude-caches --files-cache=mtime,size --patterns-from=${../borg/. + "/${name}.borg"}";
+    extraCreateArgs = "--stats --progress --exclude-caches --files-cache=mtime,size --patterns-from=${borgcfg}/${name}.borg";
     repo = "/mnt/external/borg-dir";
-    paths = ["/dev/null"];
-    startAt = [];
+    paths = [];
+    startAt = ["*-*-* 02:00:00"];
     encryption.mode = "none";
+    appendFailedSuffix = false;
+    dateFormat = "+%Yd%j";
   } // removeAttrs opts ["name"]);
-  borg-sshfs = { name, host ? opts.name, path ? ".", ... } @ opts: borg ({
+  borg-sshfs = {
+      name, touch,
+      host ? opts.name,
+      path ? ".",
+      ...} @ opts: borg ({
     preHook = ''
-      sshfs ${host}:${path} /mnt/backup -oro,reconnect
+      # Append an "+n" to duplicate archive names.
+      # So durandal-2018d123 becomes durandal-2018d123+1 the second time.
+      archiveCount="$(${pkgs.borgbackup}/bin/borg list -P "$archiveName" | ${pkgs.coreutils}/bin/wc -l)"
+      if (( archiveCount > 0 )); then
+        archiveName="$archiveName+$archiveCount"
+      fi
+
+      # Workaround for borgbackup dropping cache entries for the most recently
+      # modified files in the backup dataset, c.f.
+      # https://borgbackup.readthedocs.io/en/stable/faq.html#i-am-seeing-a-added-status-for-an-unchanged-file
+      # This also means that if we can't ssh into the host we'll abort before
+      # trying to mount it.
+      ssh ${host} touch "${path}/${touch}"
+
+      # mount the backup source
+      ${pkgs.sshfs}/bin/sshfs ${host}:${path} /mnt/backup \
+        -o ro,reconnect,workaround=rename
       cd /mnt/backup
     '';
     postHook = ''
       cd
-      fusermount -u -z /mnt/backup
+      ${pkgs.fuse}/bin/fusermount -u -z /mnt/backup
     '';
-  } // removeAttrs opts ["host" "path"]);
+  } // removeAttrs opts ["host" "path" "touch"]);
+  name-to-service = name: {
+    name = name;
+    "borgbackup-job-${name}".serviceConfig = {
+      Type = "oneshot";
+      # borg uses exit(1) for warnings like "file deleted during backup".
+      SuccessExitStatus = "1";
+    };
+  };
+  merge-and-order-services = first: second:
+    if first.name == "" then
+      second
+    else
+      lib.attrsets.recursiveUpdate
+        (first // second)
+        {
+          "borgbackup-job-${second.name}".after = ["borgbackup-job-${first.name}.service"];
+        };
+  borg-ordering = names:
+    removeAttrs
+      (builtins.foldl'
+        merge-and-order-services
+        { name = ""; }
+        (map name-to-service names))
+      ["name"];
 in {
   services.borgbackup.jobs = {
+    durandal = borg-sshfs {
+      name = "durandal";
+      path = "/.";
+      touch = "home/.borgbackup";
+    };
     "funkyhorror" = borg-sshfs {
       name = "funkyhorror";
+      touch = ".borgbackup";
     };
     "godbehere.ca" = borg-sshfs {
       name = "godbehere.ca";
+      touch = ".borgbackup";
     };
     "grandriverallbreedrescue.ca" = borg-sshfs {
-      name = "grandriverallbreedrescue.ca";
+      name = "GRABR.ca";
       host = "${secrets.grabr-user}@grandriverallbreedrescue.ca";
+      touch = ".borgbackup";
     };
-    durandal = borg-sshfs {
-      name = "durandal";
-      path = "/";
+    thoth = borg-sshfs {
+      name = "thoth";
+      path = "/.";
+      touch = "root/.borgbackup";
     };
   };
+  systemd.services = borg-ordering [
+    "thoth"
+    "durandal"
+    "godbehere.ca"
+    "grandriverallbreedrescue.ca"
+    "funkyhorror"
+  ];
 }
