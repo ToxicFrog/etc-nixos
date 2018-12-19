24bf06b81cedf910fbcf598cc9dd347a998fb832
B. Kelly <bk@ancilla.ca>
2018-12-19 18:41:42 -0500

    Improve dark theme

--- munin/munin.nix
+++ munin/munin.nix
@@ -37,15 +37,17 @@
     '';
     # Light on dark theme.
     extraCSS = ''
-      html, body {
-        background: #222222;
-      }
-      #header, #footer {
-        background: #333333;
-      }
+      html, body { background: #222222; }
+      #header, #footer { background: #333333; }
+      body, h1, h2, h3, p, span, div { color: #888888; }
       img.i, img.iwarn, img.icrit, img.iunkn {
         filter: invert(100%) hue-rotate(-30deg);
       }
+      #legend th { border-bottom: 1px solid #bbbbbb; }
+      #legend .oddrow { background-color: #222222; }
+      #legend .oddrow td { border-bottom: 1px solid #666666; }
+      #legend .evenrow { background-color: #282828; }
+      #legend .evenrow td { border-bottom: 1px solid #666666; }
     '';
   };
 
@@ -90,8 +92,8 @@
     ];
     disabledPlugins = [
       "cpuspeed"      # so noisy it's useless
-      "buddyinfo"
-      "meminfo"       # don't care about memory fragmentation
+      "buddyinfo"     # don't care about memory fragmentation
+      "meminfo"       # duplicate of above
       "diskstat_*"    # conflicts with diskstat
       "munin_stats"   # broken on NixOS
       "port_*"        # don't care about this either
