801ee303033ed9310e79a6262ad9f88a337a18ee
B. Kelly <bk@ancilla.ca>
2019-01-19 09:50:38 -0500

    ancilla: add reverse proxy for Plex

--- services/media.nix
+++ services/media.nix
@@ -29,6 +29,35 @@ in {
     nginx.virtualHosts."ancilla.lan".locations."/plex" = {
       extraConfig = "return 301 http://192.168.86.34:32400/web;";
     };
+    nginx.virtualHosts."plex.ancilla.ca" = {
+      forceSSL = true;
+      enableACME = true;
+      extraConfig = ''
+        #Forward real ip and host to Plex
+        proxy_set_header Host "192.168.86.34:32400";
+        proxy_set_header Referer "";
+        proxy_set_header Origin "http://192.168.86.34:32400";
+        #proxy_set_header X-Real-IP $remote_addr;
+        #When using ngx_http_realip_module change $proxy_add_x_forwarded_for to '$http_x_forwarded_for,$realip_remote_addr'
+        #proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        #proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header Sec-WebSocket-Extensions $http_sec_websocket_extensions;
+        proxy_set_header Sec-WebSocket-Key $http_sec_websocket_key;
+        proxy_set_header Sec-WebSocket-Version $http_sec_websocket_version;
+
+        #Websockets
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
+
+        #Buffering off send to the client as soon as the data is received from Plex.
+        proxy_redirect off;
+        proxy_buffering off;
+      '';
+      locations."/".extraConfig = ''
+        proxy_pass    http://192.168.86.34:32400;
+      '';
+    };
     nginx.virtualHosts."music.ancilla.ca" = {
       forceSSL = true;
       enableACME = true;
