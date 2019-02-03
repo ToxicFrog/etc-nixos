b6ad5ec2c872f732d7865fbb96a5893760d37d98
B. Kelly <bk@ancilla.ca>
2019-02-03 09:03:57 -0500

    Boot: adjust zpool import, install grub on nodev

--- boot.nix
+++ boot.nix
@@ -8,7 +8,8 @@
       efiSupport = true;
       efiInstallAsRemovable = true;
       version = 2;
-      device = "/dev/disk/by-id/ata-KINGSTON_SA400S37240G_50026B77824F4828";
+      device = "nodev";
+      # device = "/dev/disk/by-id/ata-KINGSTON_SA400S37240G_50026B77824F4828";
     };
     # loader.systemd-boot.enable = true;
     # loader.efi.canTouchEfiVariables = true;
@@ -17,16 +18,20 @@
     kernelModules = ["k10temp" "nct6775"];
 
     supportedFilesystems = ["zfs"];
-    zfs.extraPools = ["ancilla" "backup" "internal"];
+    #zfs.extraPools = ["ancilla" "backup" "internal"];
     zfs.devNodes = "/dev/disk/by-path";
     initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" "rtsx_pci_sdmmc" ];
     extraModulePackages = [ ];
 
     postBootCommands = ''
-      ls /srv /ancilla
-      zpool import -a -d -N /dev/disk/by-path
-      zfs mount -a
-      ls /srv /ancilla
+      echo "=== STARTING ZPOOL IMPORT ==="
+      # Clean up borg-repo because the activation script creates it
+      ${pkgs.findutils}/bin/find /backup/borg-repo -maxdepth 1 -type d -empty -delete
+      ${pkgs.zfs}/bin/zpool import -a -N -d /dev/disk/by-path
+      ${pkgs.zfs}/bin/zpool status
+      ${pkgs.zfs}/bin/zfs mount -a
+      echo "=== ZPOOL IMPORT COMPLETE ==="
+      ${pkgs.lm_sensors}/bin/sensors -c /etc/sensors3.conf -s
     '';
   };
   # systemd.targets.zfs = {
--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,7 +8,6 @@ let
 in {
   imports = [
     <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
-    ./secrets/homedirs.nix
   ];
 
   services.zfs.autoScrub = {
@@ -16,6 +15,7 @@ in {
     interval = "*-*-01 01:15:00";
   };
 
+  # Need to hard-code these so the system can start booting.
   fileSystems."/" = zfs "ancilla/root";
   fileSystems."/nix" = zfs "internal/nix";
 
@@ -23,21 +23,6 @@ in {
   # https://github.com/cleverca22/nixos-configs/blob/master/rescue_boot.nix
   # adds a boot menu entry that boots right into the NixOS installer, suitable
   # as a rescue system with more capability (probably) than the initrd.
-
-  # This is pretty gross.
-  # NixOS is unaware of ZFS mountpoints, and despite my best efforts, will try
-  # to create various home directories/start services/etc before `zfs mount -a`
-  # runs.
-  # So, we list all the filesystems that could potentially interfere with that
-  # here, and with any luck that means systemd will force them to be mounted
-  # first.
-  # Hopefully `zfs mount -a` will take care of the rest at a later stage of
-  # boot.
-  # Individual homedirs are in secrets/homedirs.nix.
-  fileSystems."/home" = zfs "ancilla/home";
-  fileSystems."/ancilla" = zfs "ancilla";
-  fileSystems."/srv" = zfs "ancilla/srv";
-  fileSystems."/ancilla/torrents" = zfs "backup/torrents";
 
   fileSystems."/boot" = {
     device = "/dev/disk/by-label/BOOT";
