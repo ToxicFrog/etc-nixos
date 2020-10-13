ab92e4af0b0b3c8318cd5dc38a4faa26118c4fb0
Rebecca Kelly <bk@ancilla.ca>
2020-10-12 20:44:20 -0400

    remove jackett override
    But keep it around just in case I need to override it again

--- overlays/default.nix
+++ overlays/default.nix
@@ -45,13 +45,13 @@
         src = /srv/airsonic/airsonic-advanced-10.6.0.war;
       });
       mavenix = super.callPackage /home/rebecca/src/mavenix {};
-      jackett = super.jackett.overrideAttrs (oldAttrs: rec {
-        version = "0.16.1021";
-        src = super.fetchurl {
-          url = "https://github.com/Jackett/Jackett/releases/download/v${version}/Jackett.Binaries.LinuxAMDx64.tar.gz";
-          sha256 = "1i0bs24q1lkxajz05fm8z5m33l1n4dsygfr2v1rqd3w2y2nykg43";
-        };
-      });
+      # jackett = super.jackett.overrideAttrs (oldAttrs: rec {
+      #   version = "0.16.1021";
+      #   src = super.fetchurl {
+      #     url = "https://github.com/Jackett/Jackett/releases/download/v${version}/Jackett.Binaries.LinuxAMDx64.tar.gz";
+      #     sha256 = "1i0bs24q1lkxajz05fm8z5m33l1n4dsygfr2v1rqd3w2y2nykg43";
+      #   };
+      # });
       jellyfin = super.jellyfin.overrideAttrs (oldAttrs: rec {
         version = "10.6.2";
         src = super.fetchurl {
