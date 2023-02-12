c7408ff17ec45d1603ed97083415378b0dda6c4a
Rebecca Kelly <bk@ancilla.ca>
2023-02-12 14:58:44 -0500

    Add overlay for an actually up to date Factor language

--- overlays/misc.nix
+++ overlays/misc.nix
@@ -18,6 +18,22 @@ in {
       sed -Ei 's,^#define MAP_DEFAULTRESET.*,#define MAP_DEFAULTRESET 28800,' include/config.h
     '';
   });
+  factor-lang = super.factor-lang.overrideAttrs (old: {
+    version = "0.99";
+    src = super.fetchFromGitHub {
+      owner = "factor"; repo = "factor";
+      rev = "d5a2d1cf1b98b9ded7fa7330cdc0e58dd01551c1";
+      sha256 = "JDIr20fiGnsMwG1rdudTHO5TTAe5ZZmoJL4TRHv5geE=";
+    };
+    bootstrap = super.fetchurl {
+      url = "https://downloads.factorcode.org/images/master/boot.unix-x86.64.image";
+      sha256 = "2+dway20pYoHgTTNGASV5R6VtyPCOhj0ntm4wyuSdlA=";
+    };
+    preBuild = ''
+      cp $bootstrap boot.unix-x86.64.image
+    '';
+    patches = [];
+  });
   ffmpeg-full = super.ffmpeg-full.overrideAttrs (old: {
     configureFlags = old.configureFlags ++ [
       "--disable-libmodplug"
