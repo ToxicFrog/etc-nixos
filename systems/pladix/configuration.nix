7b7a71f7af2f07e0f597eb939fc7005c911041c3
Rebecca Kelly <bk@ancilla.ca>
2023-05-07 10:29:17 -0400

    update pladix configuration to match what it's actually using in prod

--- systems/pladix/configuration.nix
+++ systems/pladix/configuration.nix
@@ -1,74 +1,138 @@
-# deploy with:
-# NIXOS_CONFIG=systems/pladix/configuration.nix nixos-rebuild --target-host=pladix switch
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, modulesPath, ... }:
+{ config, pkgs, ... }:
 
-let
-  unstable = import <nixos-unstable> { config.allowUnfree = true; };
-in {
-  system.stateVersion = "20.09"; # Did you read the comment?
+let unstable = import <nixos-unstable> {}; in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
-  imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
-    ../../secrets/pladix.nix
-  ];
-
-  # Use the systemd-boot EFI boot loader.
+  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.postBootCommands = ''
-    # Disable wake-on-mouse
-    echo disabled > /sys/bus/usb/devices/5-2.1/power/wakeupecho disabled > /sys/bus/usb/devices/5-2.1/power/wakeup
-  '';
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  networking = {
-    hostName = "pladix";
-    domain = "ancilla.ca";
-    networkmanager.enable = true;
-  };
+  networking.hostName = "pladix"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  #networking.interfaces.enp0s25.useDHCP = true;
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_CA.UTF-8";
-  console.font = "latarcyrheb-sun32";
-  console.keyMap = "us";
+  # Enable networking
+  networking.networkmanager.enable = true;
+  hardware.bluetooth.enable = true;
 
   # Set your time zone.
   time.timeZone = "America/Toronto";
 
-  programs.zsh.enable = true;
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_CA.utf8";
 
-  services.printing = {
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia.prime = {
+    sync.enable = true;
+    nvidiaBusId = "PCI:1:0:0";
+    intelBusId = "PCI:0:2:0";
+  }; 
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  #services.xserver.displayManager.sddm.autoLogin.relogin = true;
+  services.xserver.displayManager.autoLogin = {
     enable = true;
-    drivers = with pkgs; [ samsung-unified-linux-driver_1_00_37 ];
+    user = "pladix";
+  };
+  #systemd.services.display-manager.wants = [ "systemd-user-sessions.service" "multi-user.target" "network-online.target" ];
+  #systemd.services.display-manager.after = [ "systemd-user-sessions.service" "multi-user.target" "network-online.target" ];
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
   };
 
-  #programs.steam.enable = true;
+  # Enable openGL.
+  hardware.opengl = {
+    enable = true;
+    driSupport32Bit = true;
+    #extraPackages = with pkgs; [vaapiVdpau vaapiIntel];
+    #extraPackages32 = with pkgs; [vaapiVdpau vaapiIntel];
+  };
 
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+  services.printing.drivers = with pkgs; [ samsung-unified-linux-driver_1_00_37 ];
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.pladix = {
+    isNormalUser = true;
+    description = "pladix role account";
+    extraGroups = [ "networkmanager" "wheel" "adbusers" ];
+    shell = pkgs.zsh;
+  };
+  users.users.root.shell = pkgs.zsh;
+
+  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-  environment.systemPackages =
-  with pkgs; let
-    steam = (unstable.steam.override { extraPkgs = pkgs: with pkgs; [ pango harfbuzz libthai ]; });
-  in [
+  nixpkgs.config.packageOverrides = pkgs: {
+    steam = pkgs.steam.override {
+      extraPkgs = pkgs: with pkgs; [
+        libpng  # for dead cells
+      ];
+    };
+    waydroid = unstable.waydroid;
+  };
+  programs.steam.enable = true;
+  virtualisation.waydroid.enable = false;
+  programs.adb.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
     # system tools
     zip unzip ncdu htop xscreensaver wget ark git
     # for gaming
-    jre retroarchBare stepmania lutris scummvm wine
-    #(unstable.steam.override { extraPkgs = pkgs: with pkgs; [ pango harfbuzz libthai ]; })
-    steam steam.run
+    jre retroarchBare stepmania lutris scummvm wine itch
+    steam.run steam
     fluidsynth soundfont-fluid
-    #steam-run # for gog games, etc
     caffeine-ng # power control for retroarch
     antimicroX # controller support for keyboard-only games
+    appimage-run # for gdlauncher
+    opentyrian
+    gzdoom
     # for dbgl
     swt dosbox gsettings-desktop-schemas
     # for exodos-ll
-    unstable.dosbox-staging dialog
+    dosbox-staging dialog
     # misc games
     gnome.quadrapassel ltris lbreakout2
     # for media playback
@@ -77,12 +141,26 @@ in {
     vlc
     #libsForQt5.phonon-backend-vlc
     #libsForQt5.phonon-backend-gstreamer gst-plugins-good gst-plugins-ugly
+    # for fun
+    fortune
+    # for android stuff
+    waydroid
   ];
   nixpkgs.config.permittedInsecurePackages = [
-    "ffmpeg-2.8.17"
+    "electron-11.5.0"
   ];
 
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.zsh.enable = true;
+
   # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.openssh.forwardX11 = true;
+
+  # Enable Munin system monitor
   services.munin-node = {
     enable = true;
     extraConfig = ''
@@ -90,93 +168,18 @@ in {
     '';
   };
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  services.openssh.forwardX11 = true;
-
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
   networking.firewall.enable = false;
 
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-  hardware.pulseaudio.support32Bit = true;
-  # Fix for HDMI audio going away after suspend
-  powerManagement.resumeCommands = ''
-    sleep 2
-    ${pkgs.sudo}/bin/sudo -u pladix pacmd set-card-profile 0 output:hdmi-stereo
-  '';
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable hardware accelerated rendering.
-  hardware.opengl = {
-    enable = true;
-    driSupport32Bit = true;
-    extraPackages = with pkgs; [vaapiVdpau vaapiIntel];
-    extraPackages32 = with pkgs; [vaapiVdpau vaapiIntel];
-  };
-
-  # Enable touchpad support.
-  # services.xserver.libinput.enable = true;
-
-  # Enable the KDE Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.sddm.autoLogin.relogin = true;
-  services.xserver.displayManager.autoLogin = {
-    enable = true;
-    user = "pladix";
-  };
-  services.xserver.desktopManager.plasma5.enable = true;
-  systemd.services.display-manager.wants = [ "systemd-user-sessions.service" "multi-user.target" "network-online.target" ];
-  systemd.services.display-manager.after = [ "systemd-user-sessions.service" "multi-user.target" "network-online.target" ];
-
-  system.autoUpgrade = {
-    enable = false;
-  };
-
-  # Stuff from hardware-configuration.nix. FIXME: regenerate for pladix.
-  boot.initrd.availableKernelModules = [ "ahci" "ohci_pci" "ehci_pci" "xhci_pci" "firewire_ohci" "usb_storage" "usbhid" "sd_mod" "sr_mod" ];
-  boot.initrd.kernelModules = [ "amdgpu" ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
-
-  fileSystems."/" =
-    { device = "/dev/disk/by-label/pladix-root";
-      fsType = "ext4";
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-label/PLADIXBOOT";
-      fsType = "vfat";
-    };
-
-#  fileSystems."/data" =
-#    { device = "/dev/disk/by-uuid/5b839175-8a46-4b03-8f01-08fd74333514";
-#      fsType = "ext4";
-#    };
-
-#  fileSystems."/nix/store" =
-#    { device = "/data/nix";
-#      options = [ "bind" ];
-#    };
-
-  fileSystems."/ancilla" =
-    { device = "ancilla:/ancilla";
-      fsType = "nfs";
-    };
-
-  swapDevices = [
-    { device = "/dev/disk/by-label/pladix-swap"; }
-  ];
-
-#  nixpkgs.overlays = [
-#    (self: super: {
-#      steam = super.steam.override {
-#        extraPkgs = pkgs: with pkgs; [ pango harfbuzz libthai ];
-#      };
-#    })
-#  ];
 }
