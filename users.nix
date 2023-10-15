18f50d36b569a34dde0d21298889bc68f17caa8f
Rebecca Kelly <bk@ancilla.ca>
2023-10-15 19:51:54 -0400

    stop using our linger hack and start using support from unstable

--- users.nix
+++ users.nix
@@ -6,15 +6,5 @@ in {
   programs.zsh.enable = true;
   users.mutableUsers = false;
   users.enforceIdUniqueness = false;
-
-  users.groups.linger = {};
-  system.activationScripts.linger = ''
-    rm -rf /var/lib/systemd/linger
-    mkdir -p /var/lib/systemd/linger
-    for user in $(cat /etc/group | grep ^linger | cut -d: -f4- | tr "," " "); do
-      touch /var/lib/systemd/linger/$user
-    done
-  '';
-
   users.users = accounts;
 }
