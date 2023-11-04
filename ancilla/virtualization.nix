b1e082a5d18b8898135d11aa517cd3c11dfbfb38
Rebecca Kelly <bk@ancilla.ca>
2023-11-04 11:14:27 -0400

    static configuration for br0

--- ancilla/virtualization.nix
+++ ancilla/virtualization.nix
@@ -9,19 +9,19 @@
   environment.systemPackages = with pkgs; [ virt-manager virt-manager-qt ];
   virtualisation.libvirtd.enable = true;
   networking = {
-     bridges.br0.interfaces = [ "enp27s0" ];
-     interfaces.enp27s0.useDHCP = false;
-     interfaces.br0.useDHCP = true;
-     dhcpcd.extraConfig = ''
-       interface br0
-       dhcp
-       dhcp6
-       ipv4
-       ipv6
-
-     '';
+    bridges.br0.interfaces = [ "enp27s0" ];
+    interfaces.enp27s0.useDHCP = false;
+    # interfaces.br0.useDHCP = true;
+    interfaces.br0 = {
+      useDHCP = false;
+      ipv4.addresses = [ { address = "192.168.1.34"; prefixLength = 24; } ];
+      ipv6.addresses = [ { address = "fd85:f753:480f::7b3"; prefixLength = 128; } ];
+    };
+    defaultGateway = { address = "192.168.1.1"; interface = "br0"; };
+    nameservers = [ "192.168.1.1" ];
   };
   # We have to force this to run after br0 is created, or br0 never gets an
   # address :(
   systemd.services.dhcpcd.after = [ "br0-netdev.service" ];
+  # none of these workarounds work; we have to manually `systemctl restart network-setup` after each major rebuild :(
 }
