536fbb1510bb4d42cfef82c80a9705514ceacc70
Rebecca Kelly <bk@ancilla.ca>
2024-01-10 22:10:28 -0500

    Update Ultimate Doombuilder

--- shared/packages/ultimate-doombuilder.nix
+++ shared/packages/ultimate-doombuilder.nix
@@ -1,4 +1,5 @@
 { stdenv
+, lib
 , fetchFromGitHub
 , gtk2-x11
 , makeWrapper
@@ -7,16 +8,17 @@
 , libpng
 , libX11
 , mono
+, zdbsp
 }:
 
 stdenv.mkDerivation rec {
   pname = "UltimateDoomBuilder";
   version = "3.0.0.3274";
   src = fetchFromGitHub {
-    owner = "jewalky";
+    owner = "UltimateDoomBuilder";
     repo = pname;
-    rev = "eb974fcaf08d54c20ddc697d04f4eca84e805016";
-    hash = "sha256-vl3w1q5ssOPpPxkwmq4hKgJkA7oe8W53IYXrN4ezX7c=";
+    rev = "cff1d000b7bd51380d676e13a9319ae147a4a357";
+    hash = "sha256-uB9AfYidn7pgzB4cGP5hTPMVSTEK/G7UTIqgC7ZW5P4=";
   };
   nativeBuildInputs = [
     msbuild makeWrapper
@@ -39,12 +41,20 @@ stdenv.mkDerivation rec {
     g++ -std=c++14 -O2 --shared -g3 -o Build/libBuilderNative.so -fPIC -I Source/Native Source/Native/*.cpp Source/Native/OpenGL/*.cpp Source/Native/OpenGL/gl_load/*.c -lX11 -ldl
     runHook postBuild
   '';
+    # substituteInPlace $out/opt/UltimateDoomBuilder/Compilers/BCC/bcc.cfg --replace bcc.exe ${zt-bcc}/bin/zt-bcc
   installPhase = ''
     mkdir -p $out/bin
     mkdir -p $out/opt
+
     cp -r Build $out/opt/UltimateDoomBuilder
+
     substituteInPlace $out/opt/UltimateDoomBuilder/builder --replace mono ${mono}/bin/mono
     substituteInPlace $out/opt/UltimateDoomBuilder/builder --replace Builder.exe $out/opt/UltimateDoomBuilder/Builder.exe
-    ln -sf $out/opt/UltimateDoomBuilder/builder $out/bin/udb
+    substituteInPlace $out/opt/UltimateDoomBuilder/Compilers/Nodebuilders/zdbsp.cfg --replace zdbsp.exe ${zdbsp}/bin/zdbsp
+
+    wrapProgram $out/opt/UltimateDoomBuilder/builder \
+      --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [ gtk2-x11 libGL libpng libX11 ]}"
+
+    ln -s $out/opt/UltimateDoomBuilder/builder $out/bin/udb
   '';
 }
