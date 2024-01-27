bb809754f7243565ba95cd2415929e1fcd12dd64
Rebecca Kelly <bk@ancilla.ca>
2024-01-27 12:22:27 -0500

    Add matrix2051 IRC gateway service

--- ancilla/services/matrix.nix
+++ ancilla/services/matrix.nix
@@ -20,10 +20,35 @@
     };
   };
 
+  systemd.services.matrix2051 = {
+    description = "Matrix2051 IRC gateway for Matrix";
+    wantedBy = ["multi-user.target"];
+    after = ["network-online.target" "local-fs.target" "ancilla-media-music-ffmpegfs.service"];
+    script = ''
+      ${pkgs.matrix2051}/bin/matrix2051 start
+    '';
+    serviceConfig = {
+      Restart = "on-failure";
+    };
+    environment = {
+      RELEASE_COOKIE = "matrix2051";
+      RELEASE_TMP = "/var/empty";
+    };
+  };
+
   services.nginx.virtualHosts."matrix.ancilla.ca" = {
     enableACME = true;
     forceSSL = true;
-    locations."/_matrix/".proxyPass = "http://127.0.0.1:6167$request_uri";
+    http2 = true;
+    extraConfig = ''
+      http2_max_requests 100000;
+    '';
+    locations."/_matrix/" = {
+      proxyPass = "http://127.0.0.1:6167$request_uri";
+      extraConfig = ''
+        client_max_body_size 32M;
+      '';
+    };
     locations."= /.well-known/matrix/client" = {
       alias = pkgs.writeText "matrix-wk-client" ''
         { "m.homeserver": { "base_url": "https://matrix.ancilla.ca" } }
