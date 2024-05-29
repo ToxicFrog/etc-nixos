19ad5f6493ed1f04419d29c658ec2a0b4d6827a6
Rebecca Kelly <bk@ancilla.ca>
2024-05-28 21:55:35 -0400

    Add lix keys to the thoth configuration

--- thoth/configuration.nix
+++ thoth/configuration.nix
@@ -10,6 +10,9 @@
 
   users.users = secrets.thoth.users { inherit pkgs; };
 
+  nix.settings.extra-substituters = [ "https://cache.lix.systems" ];
+  nix.settings.trusted-public-keys = [ "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o=" ];
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
