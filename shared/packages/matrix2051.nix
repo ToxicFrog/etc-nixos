7b01afd9de2b3f216eddd3e18f44e4110af8a46f
Rebecca Kelly <bk@ancilla.ca>
2024-07-02 08:59:26 -0400

    use local matrix2051

--- shared/packages/matrix2051.nix
+++ shared/packages/matrix2051.nix
@@ -9,12 +9,13 @@ beamPackages.mixRelease rec {
   pname = "matrix2051";
   version = "0.1.0-2ee9da0";
 
-  src = fetchFromGitHub {
-    owner = "progval";
-    repo = "matrix2051";
-    rev = "2ee9da0a610233bf113eb22cb74927e8a083bda8";
-    hash = "sha256-0Xl9li7DPf2Du0adkm5+9B/gznwFkJ2qCbRAWUOkkJk=";
-  };
+  # src = fetchFromGitHub {
+  #   owner = "progval";
+  #   repo = "matrix2051";
+  #   rev = "2ee9da0a610233bf113eb22cb74927e8a083bda8";
+  #   hash = "sha256-0Xl9li7DPf2Du0adkm5+9B/gznwFkJ2qCbRAWUOkkJk=";
+  # };
+  src = /home/bex/devel/matrix2051;
 
   mixNixDeps = import ./matrix2051-mixdeps.nix {
     inherit beamPackages lib;
