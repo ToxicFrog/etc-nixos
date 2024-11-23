cae86f7bc54cb45cc9b8a9ce442f8cc4b4c71460
Rebecca Kelly <bk@ancilla.ca>
2024-11-23 17:28:11 -0500

    Add some family-shared directories to /ancilla

--- ancilla/configuration.nix
+++ ancilla/configuration.nix
@@ -45,6 +45,16 @@
     acceptTerms = true;
   };
 
+  # Shared directories on /ancilla that should be writeable by anyone even if
+  # someone else created subdirectories.
+  users.groups.parents = {};
+  system.activationScripts.shared-directory-acls = with pkgs; ''
+    ${acl}/bin/setfacl --recursive -m 'd:g:parents:rwX,g:parents:rwX' /ancilla/documents
+    ${acl}/bin/setfacl --recursive -m 'd:g:parents:rwX,g:parents:rwX' /ancilla/projects
+    ${acl}/bin/setfacl --recursive -m 'd:g:parents:rwX,g:parents:rwX' /ancilla/scans
+    ${acl}/bin/setfacl --recursive -m 'd:g:users:rwX,d:o::rX,g:users:rwX,o::rX' /ancilla/installs/games/Retroarch/games
+  '';
+
   # TODO a lot of this should be moved to packages.nix
   environment.systemPackages = with pkgs; [
     digikam # for digitaglinktree
