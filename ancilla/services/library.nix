d4ae051e40387877a930c446dd0894cbaed2c15a
Rebecca Kelly <bk@ancilla.ca>
2024-12-03 07:40:58 -0500

    Switch codex to local build + deploy rather than docker

--- ancilla/services/library.nix
+++ ancilla/services/library.nix
@@ -45,21 +45,34 @@
     uid = 987;
   };
   users.groups.codex = { gid = 980; };
-  virtualisation.oci-containers.containers.codex = {
-    # image = "ajslater/codex@sha256:1faf2ca1c75ec09903fe6a3c902e1ce3622b6e17d3223fbc0631dc056b488416"; # 1.5.0 rc2
-    image = "ajslater/codex@sha256:57f77e79441b0b200c6c17339a89b6448281df6f645f86b6fb39870dba2db492"; # 1.4.3
-    user = "987:980";
-    ports = ["9810:9810"];
-    extraOptions = ["--memory=1g"];
-    volumes = [
-      "/var/lib/codex:/config"
-      "/var/lib/codex:/.config"
-      "/ancilla/media/comics:/ancilla/media/comics:ro"
-      "/ancilla/media/books:/ancilla/media/books:ro"
-    ];
+
+  # Codex is installed via pip, because docker is full of spiders.
+  # This is done as the codex user (sudo -u codex bash),
+  # in a nix shell (nix-shell -p python3Packages.{pip,virtualenv}).
+  # The venv is initialized with:
+  # $ virtualenv codex-venv
+  # Codex is then installed with:
+  # $ codex-venv/bin/install codex
+  # And then executed with:
+  # $ codex-venv/pip/codex
+  systemd.services.codex = {
+    description = "codex comic server";
+    wantedBy = ["multi-user.target"];
+    after = ["network-online.target" "local-fs.target"];
+    requires = ["network-online.target" "local-fs.target"];
+    script = ''
+      codex-venv/pip/codex
+    '';
     environment = {
-      TZ = "America/Toronto";
-      LOGLEVEL = "INFO";
+      # Needed so libmupdf, which comes with codex, can find libstd++.
+      LD_LIBRARY_PATH = "/run/current-system/sw/share/nix-ld/lib";
+    };
+    serviceConfig = {
+      User = "codex";
+      Group = "codex";
+      WorkingDirectory = "~";
+      Restart = "always";
+      RestartSec = "30";
     };
   };
 }
